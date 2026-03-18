0efb2ec4e3b69f382edfd6d1665b360650b5e8f3
Nice3point <nice3point@gmail.com>
2026-03-19 01:50:36 +0300

    [Extensions] Add Document extensions

---
+++ source/Nice3point.Revit.Extensions/DocumentExtensions.cs
@@ -0,0 +1,35 @@
+﻿using JetBrains.Annotations;
+
+namespace Nice3point.Revit.Extensions;
+
+/// <summary>
+///     Revit Document Extensions
+/// </summary>
+[PublicAPI]
+public static class DocumentExtensions
+{
+    /// <param name="document">The source document</param>
+    extension(Document document)
+    {
+        /// <summary>Gets the DocumentVersion that corresponds to a document.</summary>
+        /// <remarks>
+        ///    This property can be combined with <see cref="P:Autodesk.Revit.DB.Document.IsModified" />
+        ///    to see whether a document in memory is different from a version on disk. The documents
+        ///    are different if the document is modified or if the two DocumentVersions differ.
+        /// </remarks>
+        /// <returns>The DocumentVersion corresponding to the given document.</returns>
+        public DocumentVersion Version => Document.GetDocumentVersion(document);
+#if REVIT2023_OR_GREATER
+
+        /// <summary>
+        ///    Checks whether the GUID is valid for the given document. Empty GUID is allowed.
+        /// </summary>
+        /// <param name="versionGuid">The GUID to check.</param>
+        /// <returns>True if the GUID is valid.</returns>
+        public bool IsValidVersionGuid(Guid versionGuid)
+        {
+            return Document.IsValidVersionGUID(document, versionGuid);
+        }
+#endif
+    }
+}
