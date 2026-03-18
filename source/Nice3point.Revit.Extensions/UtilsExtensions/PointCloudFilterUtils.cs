18a0b8f4ab122bbc6be74c5c7c058ce2a4e3879a
Nice3point <nice3point@gmail.com>
2026-03-19 00:21:12 +0300

    [Extensions] Add PointCloudFilterUtils extensions

---
+++ source/Nice3point.Revit.Extensions/UtilsExtensions/PointCloudFilterUtils.cs
@@ -0,0 +1,32 @@
+﻿using Autodesk.Revit.DB.PointClouds;
+using JetBrains.Annotations;
+
+// ReSharper disable once CheckNamespace
+namespace Nice3point.Revit.Extensions.PointClouds;
+
+/// <summary>
+///     Represent extension methods for the <see cref="Autodesk.Revit.DB.PointClouds.PointCloudFilterUtils"/> class.
+/// </summary>
+[PublicAPI]
+public static class PointCloudFilterUtilsExtensions
+{
+    /// <param name="filter">The source point cloud filter.</param>
+    extension(PointCloudFilter filter)
+    {
+        /// <summary>Computes outline of a part of a box that satisfies given PointCloudFilter.</summary>
+        /// <param name="box">A box aligned with coordinate axes.</param>
+        /// <returns>
+        ///    The bounding box of the set of all points within the original box that satisfy the filter.
+        ///    Not every point within the resulting outline satisfies the filter, but any point that is contained
+        ///    in the original box and satisfies the filter is guaranteed to be within the resulting outline.
+        /// </returns>
+        /// <exception cref="T:Autodesk.Revit.Exceptions.ArgumentNullException">
+        ///    A non-optional argument was null
+        /// </exception>
+        [Pure]
+        public Outline GetFilteredOutline(Outline box)
+        {
+            return PointCloudFilterUtils.GetFilteredOutline(filter, box);
+        }
+    }
+}
