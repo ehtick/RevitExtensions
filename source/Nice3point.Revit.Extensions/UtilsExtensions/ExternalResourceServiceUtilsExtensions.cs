76f00721db5da4d7966a8212ee73ed03a5c54c92
Nice3point <nice3point@gmail.com>
2026-03-19 00:20:01 +0300

    [Extensions] Add ExternalResourceServiceUtils extensions

---
+++ source/Nice3point.Revit.Extensions/UtilsExtensions/ExternalResourceServiceUtilsExtensions.cs
@@ -0,0 +1,25 @@
+﻿using JetBrains.Annotations;
+
+// ReSharper disable once CheckNamespace
+namespace Nice3point.Revit.Extensions;
+
+/// <summary>
+///     Represent extension methods for the <see cref="Autodesk.Revit.DB.ExternalResourceServiceUtils"/> class.
+/// </summary>
+[PublicAPI]
+public static class ExternalResourceServiceUtilsExtensions
+{
+    /// <param name="resourceType">The source external resource type.</param>
+    extension(ExternalResourceType resourceType)
+    {
+        /// <summary>
+        ///    Gets registered external resource servers which support the external resource type.
+        /// </summary>
+        /// <returns>A list of matched external resource servers</returns>
+        [Pure]
+        public IList<IExternalResourceServer> GetServers()
+        {
+            return ExternalResourceServiceUtils.GetServersByType(resourceType);
+        }
+    }
+}
